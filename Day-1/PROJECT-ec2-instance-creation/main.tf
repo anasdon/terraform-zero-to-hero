cb3b3b3b8f97c061171fa12ef7202f0f9bb397cd
anasdon <141217172+anasdon@users.noreply.github.com>
2024-07-30 20:01:42 -0700

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,22 @@
+terraform {
+required_provider {
+ aws = {
+   source = "hashcorp/aws"
+   version = "~> 3.11.9"
+   }
+ }
+
+required_version = ">= 1.2.0"
+
+}
+
+# Set your desired AWS region
+
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "us-east-1"  
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-028362553645"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
